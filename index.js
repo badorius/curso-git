f9bc6352752d9dc7d53f8b0a06417d6c04a25bf4
badorius <salvadormuntane@gmail.com>
2022-01-30 19:52:14 +0100

    Feature videos (#6)
    * Cambios videos 1
    
    * Cambios videos 2
    
    * Cambios videos 3
    
    * Cambios videos 3

--- index.js
+++ index.js
@@ -68,8 +68,8 @@ const affiliateRedirect = () => {
 // cambio 2 - 2
 // cambio 3 - 3 
 
-// cambios videos 1
-// cambios videos 2
-// cambios videos 3
+// cambios videos 1 - 1
+// cambios videos 2 - 2
+// cambios videos 3 - 3.
 // cambios videos 4
 // cambios videos 5
